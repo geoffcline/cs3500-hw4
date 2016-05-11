2149d8c11666d7f330d05bc42e2132b676522c1c
Geoffrey Cline <gdcwd5@mst.edu>
2016-05-11 17:42:44 -0500

    function stuff not wokring

--- myhw4.lisp
+++ myhw4.lisp
@@ -22,32 +22,35 @@
 	(cond
 		((eq L nil) nil)
 		((eq (car L) X) (myRemove X (cdr L)))
-		((not (eq (car L) X)) (list (car L) (myRemove X (cdr L))))
+		((not (eq (car L) X)) (cons (car L) (myRemove X (cdr L))))
 	)
 )
 
 (defun myPurge (L)
-	(list (car L) (myPurge (myRemove X (cdr L))))
+	(cond 
+		((eq L nil) nil)
+		((not (eq L nil)) (cons (car L) (myPurge (myRemove (car L) (cdr L)))))
+	)
 )
 
 (defun myCommon (L1 L2)
 	(cond 
 		((eq L1 nil) (nil))
-		((eq myMember((car L1) L2) t) (list (car L1) (myCommon (cdr L1) L2)))
-		((not (eq myMember((car L1) L2) t)) (myCommon (cdr L1) L2))
+		((eq (myMember (car L1) L2) t) (cons (car L1) (myCommon (cdr L1) L2) ) )
+		((not (eq (myMember (car L1) L2) t)) (myCommon (cdr L1) L2))
 	)
 )
 
 (defun myGen (X Y Z)
 	(cond 
-		((<= (+ X Y) Z) (list X (myGen (+ X Y) Y Z)))
+		((<= (+ X Y) Z) (cons X (myGen (+ X Y) Y Z)))
 		((> (+ X Y) Z) (nil))
 	)
 )
 
 (defun myMap (F L)
 	(cond
-		((not (eq L nil)) (list (F (car L)) (myMap F (cdr L))))
+		((not (eq L nil)) (cons (F (car L)) (myMap F (cdr L))))
 		((eq L nil) (nil))
 	)
 )
